4d431e80aacbbeeb61666440a4bf62716b5666e9
Aditya99880 <98234782+Aditya99880@users.noreply.github.com>
2024-02-13 19:05:29 +0530

    Time: 51 ms (43.20%) | Memory: 23.2 MB (75.99%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    bool check(string &s)
-    {
-        int n=s.size()-1;
-        for(int i=0;i<n/2+1;i++)
-        {
-            if(s[i]!=s[n-i]) return false;
-        
-        }
-        return true;
-    }
+   
     string firstPalindrome(vector<string>& words) {
-        for(auto &i:words)
+        int i,f=0;
+        for(auto &j:words)
         {
-            if(i[0]==i[i.size()-1]){
-                if(check(i)) return i;
+            f=0;
+            int n=j.size()-1;
+            for(i=0;i<n/2+1;i++){
+                if(j[i]!=j[n-i]){
+                    f=-1;
+                    break;
+                }
             }
+            if(f==0)
+             return j;
         }
         return "";
     }

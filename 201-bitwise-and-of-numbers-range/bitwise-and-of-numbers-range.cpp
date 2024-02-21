314a398541367505be8c2833155512bb5a1e8650
Aditya99880 <98234782+Aditya99880@users.noreply.github.com>
2024-02-21 17:04:20 +0530

    Time: 4 ms (79.27%) | Memory: 8.8 MB (26.31%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+    
+    // Find the common prefix of left and right in binary representation
+    while (left != right) {
+        left >>= 1;
+        right >>= 1;
+        shift++;
+    }
+    
+    // Left-shift the common prefix to the original position
+    return left << shift;
+
+    }
+};

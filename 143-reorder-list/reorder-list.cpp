dec50acd647e5aee4c4bcf7ff5d197e5e5414caa
Aditya99880 <98234782+Aditya99880@users.noreply.github.com>
2024-03-23 09:47:07 +0530

    Time: 29 ms (25.65%) | Memory: 21.2 MB (69.02%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+    
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return;
+
+        // Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* secondHalf = slow->next;
+        slow->next = nullptr;
+
+        // Reverse the second half
+        secondHalf = reverseList(secondHalf);
+
+        // Interleave the nodes
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalf;
+        while (p1 && p2) {
+            ListNode* next1 = p1->next;
+            ListNode* next2 = p2->next;
+            p1->next = p2;
+            p2->next = next1;
+            p1 = next1;
+            p2 = next2;
+        }
+    }
+};
